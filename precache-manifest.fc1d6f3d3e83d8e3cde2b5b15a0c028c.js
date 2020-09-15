39af2b4baabdd1e4454cdfc40fde504a7d7d3179
Luke Lewis <lukemarius@gmail.com>
2020-09-15 16:28:21 -0700

    Updates

---
+++ precache-manifest.fc1d6f3d3e83d8e3cde2b5b15a0c028c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd82f2c3c3a66b4286410b3914523fda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe34b36c87b3eeec0944",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9f1e2d5e4705b30812b0",
+    "url": "/static/js/2.8355e615.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.8355e615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe34b36c87b3eeec0944",
+    "url": "/static/js/main.7c6b9de0.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
